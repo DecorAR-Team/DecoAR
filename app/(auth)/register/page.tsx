3090e2a6a3391da5efce7b80235cc641436ee936
Sind96 <sindhujany@gmail.com>
2024-06-24 16:00:49 +0100

    login in page finished 80%, register page needs tailwind

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,54 @@
+'use client';
+
+import LoginHeaders from "@/components/loginHeaders";
+const { useState } = require("react");
+
 export default function Register() {
+  const loginText = 'DecoAR';
+  const messageText = 'Start visualizing your dream room';
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword ] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  
   return (
     <main>
-      <h1>Register</h1>
-    </main>
+
+  <LoginHeaders Login={loginText} Message={messageText}/>
+
+  <form >
+    <input 
+    type="text" 
+    placeholder="Username"
+    id="username"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)} 
+    />
+    <input type="text" 
+    placeholder="Email"
+    id="email"
+    value={email}
+    onChange={(e) => setEmail(e.target.value)} />
+    <input type="password" 
+    placeholder="Password"
+    id="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)} />
+    <input type="password" 
+    placeholder="Confirm Password"
+    id="confirmPassword"
+    value={confirmPassword}
+    onChange={(e) => setConfirmPassword(e.target.value)} />
+    <button type="submit">Create Account</button>
+</form>
+
+  <div>
+    <span>By continuing, you agree to the Terms of Use. Read our Privacy Policy.</span>
+    <p>{`Already have an account?`}</p>
+      <span>Login</span>
+  </div>
+</main>
   );
 }
