5310a4d4ca4a724d7e7a841a034b26fdb49fa950
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-06-25 18:59:26 +0200

    fix: update prisma schema

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,17 @@ const prisma = new PrismaClient();
 
 export default async function Home() {
   const cats = await prisma.subcategoryWithProductIds.findMany();
-  console.log(cats);
+  // console.log(cats);
 
-  // const categoryWithSubcategories = await prisma.category.findUnique({
-  //   where: { id: '66798544ee251e9881be4060' },
-  //   include: { subcategories: true },
+  // const categoryWithSubcategories = await prisma.subcategory.findUnique({
+  //   where: { id: '667aac74547fb73491fc8d0a' },
+  //   include: { category: true },
   // });
   // console.log(categoryWithSubcategories);
+
+  // const productWith3d = await prisma.productWith3d.findUnique({
+  //   where: { id: '667ad8c6547fb73491fc9140' },
+  // });
 
   return <div>Check the console</div>;
 }
