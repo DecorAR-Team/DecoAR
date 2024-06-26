d3deaf86bd0ee903752dd67479b542d10eebd141
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-06-26 23:54:11 +0200

    feat: add server actions

---
+++ app/lib/data.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
+
+export async function fetchCategories() {
+  try {
+    const categories = await prisma.category.findMany();
+    return categories;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch categories.');
+  }
+}
+
+export async function fetchSubCategories() {
+  try {
+    const subcategories = await prisma.subcategory.findMany();
+    return subcategories;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch categories.');
+  }
+}
