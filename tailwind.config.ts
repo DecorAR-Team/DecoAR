913043e14956e7ff21815c7233f42c964745a69b
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-06-26 23:55:16 +0200

    feat: upd config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         'custom-bg': '#f7fafc',
         'custom-button': '#1469C2',
         'custom-input': '#E8EDF2',
         'custom-input-text': '#4F7396',
-        'custom-logout' : '#F3F2F0'
+        'custom-logout': '#F3F2F0',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar::-webkit-scrollbar': {
+          /* Hide scrollbar for Chrome, Safari and Opera */ display: 'none',
+        },
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none' /* IE and Edge */,
+          'scrollbar-width': 'none' /* Firefox */,
+        },
+      });
+    }),
+  ],
 };
 export default config;
