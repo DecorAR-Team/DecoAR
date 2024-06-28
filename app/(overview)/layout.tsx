bf2983d0adade78778027ae71204e358a53c3c97
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-06-28 23:46:10 +0200

    fixt: layout only has bottom navbar; search ui component is on home page and search page

--- app/(overview)/layout.tsx
+++ app/(overview)/layout.tsx
@@ -10,16 +10,10 @@ export default function Layout({
   // console.log(searchParams);
 
   return (
-    <div className="flex h-screen flex-col">
-      <div className="px-6 py-6 w-full flex-none md:w-64 md:mx-auto">
-        <Search placeholder="Search" />
-      </div>
-      <div className="px-6 flex-grow md:overflow-y-auto md:p-12">
-        {children}
-      </div>
-      <div className="w-full flex-none md:w-64">
-        <Navbar />
-      </div>
+    <div className="flex flex-col">
+      {/* TODO do we need h-dvh class on wrapper? */}
+      <div className="px-6 flex-grow ">{children}</div>
+      <Navbar />
     </div>
   );
 }
