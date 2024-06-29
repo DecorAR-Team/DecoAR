29e74599d78bd9246edf53a457258ab03e564bac
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-06-29 12:44:48 +0200

    fix: search redirect

--- app/(overview)/search/page.tsx
+++ app/(overview)/search/page.tsx
@@ -3,7 +3,7 @@ import Search from '@/app/ui/search';
 export default function SearchPage() {
   return (
     <div className="py-6 w-full flex-none">
-      <Search placeholder="Search" />
+      <Search placeholder="Search" redirectOnFocus={true} />
     </div>
   );
 }
