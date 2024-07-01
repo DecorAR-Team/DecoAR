a2e6e9cc53eff242a1066fc406fbd53f5aa02cac
Sulo Siigur <sulo.siigur@gmail.com>
2024-07-01 14:03:57 +0100

    testing: tailwind issue

--- app/product/[id]/details/3d/Threedee.tsx
+++ app/product/[id]/details/3d/Threedee.tsx
@@ -3,8 +3,21 @@
 import * as THREE from 'three'
 import { createRoot } from 'react-dom/client'
 import React, { useRef, useState } from 'react'
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { Canvas, useFrame, ThreeElements, useLoader } from '@react-three/fiber'
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
-export default function Canvas() {
-  
+export default function Threedee({id}: {id: string}) {
+  // console.log(id)
+  // const threeObj = useLoader(OBJLoader, '3d/assets/armchair.obj')
+  return (
+    <Canvas>
+      <mesh>
+        {/* <primitive object={threeObj}/> */}
+        <sphereGeometry args={[2, 16, 16]} />
+        <meshStandardMaterial color="purple"/>
+      </mesh>
+    </Canvas>
+  )
 }
+
+// createRoot(document.getElementById('root')).render(<App />)
--- app/product/[id]/details/3d/page.tsx
+++ app/product/[id]/details/3d/page.tsx
@@ -3,7 +3,7 @@ import Threedee from "./Threedee";
 export default function Canvas3D({params}: {params: {id: string}}) { //pass params as a prop to child component / fetch URL from server before passing
   const objectID = params.id;
     return (
-          <div>
+          <div className="h-dvh w-full">
               <Threedee id={objectID}/>
           </div>
     )
