4e20d223e9cecff27d8e3735cf175a43ec8e3db4
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-07-01 11:45:19 +0200

    Feature: changes on components to work with CLerk

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -8,63 +8,79 @@ import {
   WalletIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { getUserSession } from '../lib/session';
-import Image from 'next/image';
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  useSession,
+  useAuth
+} from '@clerk/nextjs'
 
-export default function Navbar({ user }: { user: any }) {
+
+export default function Navbar() {
+  const { session } = useSession();
+  console.log(session?.user)
+
+  const {user } = useUser();
+  console.log(user?.firstName)
+  // console.log(user)
+
+  
+  const { isSignedIn, userId } = useAuth();
+console.log(isSignedIn, userId)
+
+
   const navlinks = [
     {
       name: 'Home',
       href: '/',
       icon: HomeIcon,
     },
-    // {
-    //   name: 'Wallet',
-    //   href: user ? '/profile/wallet' : 'api/auth/signin',
-    //   icon: WalletIcon,
-    // },
+    {
+      name: 'Wallet',
+      href: '/profile/wallet',
+      icon: WalletIcon,
+    },
     { name: 'Browse', href: '/search', icon: MagnifyingGlassCircleIcon },
     {
       name: 'Favorites',
-      href: user ? '/profile/favorites' : 'api/auth/signin',
+      href: '/profile/favorites',
       icon: HeartIcon,
     },
-    {
-      name: user ? user.name : 'Profile',
-      href: user ? '/profile' : 'api/auth/signin',
-      icon: UserCircleIcon,
-      bgImage: user ? user.image : '',
-    },
+    // {
+    //   name: user ? user.name : 'Profile',
+    //   href:  '/profile',
+    //   icon: UserCircleIcon,
+    //   bgImage: user ? user.image : '',
+    // },
   ];
 
   return (
     <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-slate-200 dark:bg-slate-700 dark:border-slate-600">
-      <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+      <div className="grid h-full max-w-lg grid-cols-5 mx-auto font-medium">
         {navlinks.map((link) => {
           const LinkIcon = link.icon;
           return (
-            <Link
-              key={link.name}
-              href={link.href}
-              className="inline-flex flex-col gap-2 items-center justify-center px-5 hover:bg-slate-50 dark:hover:bg-slate-800 group  py-2"
-            >
-              {user && link.name === user.name ? ( //TODO: more elegant way to check if the LinkIcon is Profile and user is logged in, to show userImg?
-                <Image
-                  src={user.image!}
-                  alt={user.name!}
-                  width={20}
-                  height={20}
-                  className="rounded-full"
-                />
-              ) : (
+              <Link
+                key={link.name}
+                href={link.href}
+                className="inline-flex flex-col gap-2 items-center justify-center px-5 hover:bg-slate-50 dark:hover:bg-slate-800 group  py-2"
+              >
                 <LinkIcon className="aria-hidden size-5 text-slate-500 dark:text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-500" />
-              )}
-              <span className="text-sm text-slate-500 dark:text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-500]">
-                {link.name}
-              </span>
-            </Link>
+                <span className="text-sm text-slate-500 dark:text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-500]">
+                  {link.name}
+                </span>
+              </Link>
           );
         })}
+        <SignedOut>
+          <SignInButton />  
+        </SignedOut>
+        <SignedIn >
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
