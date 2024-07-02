a6596300e7ec227e9579652dd19a538ad463e98f
Sulo Siigur <sulo.siigur@gmail.com>
2024-07-02 11:18:07 +0100

    added: use state

--- app/product/[id]/details/3d/page.tsx
+++ app/product/[id]/details/3d/page.tsx
@@ -1,37 +1,26 @@
+import Qrcode from '@/app/ui/qrpage/qrcode';
 import Threedee from './Threedee';
 import Navbar from '@/app/ui/navbar';
 import Link from 'next/link';
 import { IoMdArrowBack } from 'react-icons/io';
 import { TbAugmentedReality } from "react-icons/tb";
-import QRCode from 'qrcode';
+
 
 export default async function Canvas3D({ params }: { params: { id: string } }) {
   const objectID = params.id;
 
-  // With async/await
-  const generateQR = async (text) => {
-    try {
-      return await QRCode.toDataURL(text)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  const qrcode = await generateQR("https://www.youtube.com")
-
-  let isToggled = false;
-
-  const toggleQRCode = () => {
-    isToggled = !isToggled;
-  }
   return (
     <div>
       <div className='flex justify-between'>
         <Link href={`/product/${objectID}/details`}>
             <IoMdArrowBack className="text-2xl sm:text-3xl mt-5" />
         </Link>
-            <button onClick={toggleQRCode} >
+            <button >
               <TbAugmentedReality className="text-3xl sm:text-3xl mt-5"/>
             </button>
+      </div>
+      <div>
+        <Qrcode image={`https://www.youtube.com`} />
       </div>
       <div className="h-dvh w-full sm:w-dvw">
         <Threedee id={objectID} />
--- app/ui/qrpage/qrcode.tsx
+++ app/ui/qrpage/qrcode.tsx
@@ -1,7 +1,29 @@
+'use client'
+
 import Image from "next/image";
+import QRCode from 'qrcode';
+import { useState } from "react";
 
 
 export default function Qrcode({image}: {image: string}) {
+  const [qrImage, setQrImage] = useState('' as string)
+
+  QRCode.toDataURL(image)
+  .then(url => {
+    setQrImage(url)
+    return(url)
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
+  console.log(qrImage)
+  let canvas = document.getElementById('canvas') 
+
+  QRCode.toCanvas(canvas, qrcode, function (error) {
+    if (error) console.error(error)
+    console.log('success!');
+  })
 
   return (
     <div>
