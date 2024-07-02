9bb9f4230a273e0695c202d767e3ded80ddda1f2
Sulo Siigur <sulo.siigur@gmail.com>
2024-07-02 10:04:04 +0100

    added: basic styles

--- app/product/[id]/details/3d/page.tsx
+++ app/product/[id]/details/3d/page.tsx
@@ -1,10 +1,23 @@
-import Threedee from "./Threedee";
+import Threedee from './Threedee';
+import Navbar from '@/app/ui/navbar';
+import Link from 'next/link';
+import { IoMdArrowBack } from 'react-icons/io';
+import { TbAugmentedReality } from "react-icons/tb";
 
-export default async function Canvas3D({params}: {params: {id: string}}) { //pass params as a prop to child component / fetch URL from server before passing
+export default async function Canvas3D({ params }: { params: { id: string } }) {
   const objectID = params.id;
-    return (
-          <div className="h-dvh w-full">
-              <Threedee id={objectID} />
-          </div>
-    )
+  return (
+    <div>
+      <div className='flex justify-between'>
+        <Link href={`/product/${objectID}/details`}>
+            <IoMdArrowBack className="text-2xl sm:text-3xl mt-5" />
+        </Link>
+        <TbAugmentedReality className="text-3xl sm:text-3xl mt-5"/>
+      </div>
+      <div className="h-dvh w-full sm:w-dvw">
+        <Threedee id={objectID} />
+      </div>
+      <Navbar />
+    </div>
+  );
 }
