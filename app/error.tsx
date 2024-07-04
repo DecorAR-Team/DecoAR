b1e0d5f9a8eb2f2f86096abc4059c22e8c904d85
Sind96 <sindhujany@gmail.com>
2024-07-04 09:27:49 +0100

    Update: Error message changed

--- app/error.tsx
+++ app/error.tsx
@@ -5,8 +5,9 @@ import ArrowBack from "@/components/ui/arrow-back";
 export default function Custom404() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6 text-center">
-      <h1 className="text-6xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="text-2xl mb-6">Sorry, the page you are looking for does not exist.</p>
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <h2 className="text-4xl font-bold mb-4">Looks like you're lost!</h2>
+      <p className="text-2xl mb-6">The page you are looking for does not exist.</p>
       <ArrowBack />
     </div>
   );
